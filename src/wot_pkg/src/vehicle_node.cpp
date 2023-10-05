bea9c978368f8bd582c01f8cf8b4e9d576cf86c9
Sanyinagy <sandornagy@edu.bme.hu>
2023-10-05 12:33:41 +0200

    Create markers for bounding box

--- src/wot_pkg/src/vehicle_node.cpp
+++ src/wot_pkg/src/vehicle_node.cpp
@@ -3,15 +3,14 @@
 #include <std_msgs/String.h>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <visualization_msgs/Marker.h>
 
 geometry_msgs::PoseStamped actual_pose;
 
 
 void odomCallBack(const nav_msgs::Odometry msg)
-	{
-		
+	{		
         actual_pose.pose = msg.pose.pose;
-
 	}
 
 
@@ -27,8 +26,29 @@ int main(int argc, char **argv)
     n.param<std::string>("/" + vehicle_name + "/position" , pose_topic_name, "/" + vehicle_name + "/position");
 
     ros::Subscriber sub = n.subscribe(odom_topic_name, 1000, odomCallBack);
-    ros::Publisher pub = n.advertise<geometry_msgs::PoseStamped>(pose_topic_name, 1000);        
+    ros::Publisher pub = n.advertise<geometry_msgs::PoseStamped>(pose_topic_name, 1000);   
+    ros::Publisher vis_pub = n.advertise<visualization_msgs::Marker>( "visualization_marker", 0 );     
     ros::Rate r(10);
+
+    visualization_msgs::Marker cluster_marker;
+    cluster_marker.header.stamp = ros::Time();			
+    cluster_marker.header.frame_id = vehicle_name + "/base_link";			
+    cluster_marker.action = visualization_msgs::Marker::ADD;			
+    cluster_marker.lifetime = ros::Duration(0.5);			
+    cluster_marker.ns = "my_namespace";			
+    cluster_marker.type = visualization_msgs::Marker::CUBE;			
+    cluster_marker.pose.position.x = 0;			
+    cluster_marker.pose.position.y = 0;			
+    cluster_marker.pose.position.z = 0;		
+    cluster_marker.pose.orientation.w = 1.0;			
+    cluster_marker.color.r = 0.0;			
+    cluster_marker.color.g = 0.0;			
+    cluster_marker.color.b = 1.0;			
+    cluster_marker.color.a = 1.0;			
+    cluster_marker.scale.x = 0.25;			
+    cluster_marker.scale.y = 0.2;			
+    cluster_marker.scale.z = 0.15;
+
 
     while (ros::ok())
     {
@@ -36,7 +56,7 @@ int main(int argc, char **argv)
         geometry_msgs::PoseStamped msg;
         msg.pose = actual_pose.pose;
         pub.publish(msg);           
-        
+        vis_pub.publish(cluster_marker);
 
         ros::spinOnce();
         r.sleep();
